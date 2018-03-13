55e30861580d170a082d242f80f4addeb019c95b
mgrcar <miha@sowalabs.com>
2018-03-13 15:53:39 +0100

    Some more cleanup

--- EmptyCoreMvcProject/Controllers/HomeController.cs
+++ EmptyCoreMvcProject/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using EmptyCoreMvcProject.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace EmptyCoreMvcProject.Controllers
 {
@@ -15,23 +9,9 @@ namespace EmptyCoreMvcProject.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
--- EmptyCoreMvcProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EmptyCoreMvcProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
